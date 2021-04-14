91c0fe8a5f45d007e56519e5216b17f6e5b7a26e
Carlos Henrique <carlosheng6@gmail.com>
2021-04-14 18:32:21 -0400

    feat: implementa classe de serviço para carra

---
+++ src/main/java/com/carros/domain/CarroService.java
@@ -0,0 +1,27 @@
+package com.carros.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarroService {
+
+    @Autowired
+    private CarroRepository rep;
+
+    public Iterable<Carro> getCarros(){
+        return rep.findAll();
+    }
+
+    public List<Carro> getCarrosFake(){
+        List<Carro> carros = new ArrayList<>();
+        carros.add(new Carro(1L, "Fusca"));
+        carros.add(new Carro(2L, "Brasilia"));
+        carros.add(new Carro(3L, "Chevette"));
+
+        return  carros;
+    }
+}
