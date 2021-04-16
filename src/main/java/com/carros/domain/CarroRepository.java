fc3b43f1cae460d0feb43003ba81d19b1adae09a
Carlos Henrique <carlosheng6@gmail.com>
2021-04-15 20:07:28 -0400

    fix: altera extends

--- src/main/java/com/carros/domain/CarroRepository.java
+++ src/main/java/com/carros/domain/CarroRepository.java
@@ -1,6 +1,10 @@
 package com.carros.domain;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface CarroRepository extends CrudRepository<Carro, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CarroRepository extends JpaRepository<Carro, Long> {
+    List<Carro> findByTipo(String tipo);
 }
