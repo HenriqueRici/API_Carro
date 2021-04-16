3e70cf6d97337277f24f5ed664f0d7d0f5d9a76a
Carlos Henrique <carlosheng6@gmail.com>
2021-04-15 20:06:19 -0400

    refactor: altera retornor para CarroDTO

--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -1,10 +1,13 @@
 package com.carros.domain;
 
+import com.carros.domain.dto.CarroDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarroService {
@@ -12,16 +15,56 @@ public class CarroService {
     @Autowired
     private CarroRepository rep;
 
-    public Iterable<Carro> getCarros(){
-        return rep.findAll();
+    public List<CarroDTO> getCarros(){
+        List<Carro> carros = rep.findAll();
+        List<CarroDTO> list = new ArrayList<>();
+
+        for (Carro c: carros){
+            list.add(CarroDTO.create(c));
+        }
+         return list;
     }
 
-    public List<Carro> getCarrosFake(){
-        List<Carro> carros = new ArrayList<>();
-        carros.add(new Carro(1L, "Fusca"));
-        carros.add(new Carro(2L, "Brasilia"));
-        carros.add(new Carro(3L, "Chevette"));
+    public Optional<CarroDTO> getCarroById(Long id) {
+        return rep.findById(id).map(CarroDTO::create);
+    }
 
-        return  carros;
+    public List<CarroDTO> getCarroByTipo(String tipo) {
+        List<Carro> carros = rep.findByTipo(tipo);
+        List<CarroDTO> list = new ArrayList<>();
+
+        for (Carro c: carros){
+            list.add(CarroDTO.create(c));
+        }
+        return list;
+    }
+
+    public CarroDTO insert(Carro carro) {
+        return  CarroDTO.create(rep.save(carro));
+    }
+
+    public CarroDTO update(Carro carro, Long id) {
+        Assert.notNull(id, "Não é possivel atualizar o registro");
+
+        Optional<Carro> optional = rep.findById(id);
+        if (optional.isPresent()){
+            Carro db = optional.get();
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+
+            rep.save(db);
+
+            return CarroDTO.create(db);
+        }else {
+            return null;
+        }
+    }
+
+    public boolean delete(Long id) {
+        if (getCarroById(id).isPresent()){
+            rep.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
