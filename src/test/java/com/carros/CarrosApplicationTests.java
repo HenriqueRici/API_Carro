9d7dcafef7cc0917aa7faf33e0cf852662707045
Carlos Henrique <carlosheng6@gmail.com>
2021-05-01 13:03:26 -0400

    feat: implementado classe de testes

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -1,13 +1,23 @@
 package com.carros;
 
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+    @Autowired
+    private CarroService service;
+
     @Test
-    void contextLoads() {
+    void test1() {
+        Carro carro = new Carro();
+        carro.setNome("Ferrari");
+        carro.setTipo("Esportivos");
+        service.insert(carro);
     }
 
 }
